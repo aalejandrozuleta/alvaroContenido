4ed9b163137cf7bda49012bbf410b8a7c15ac227
jeffer <jefferdcondiza@gmail.com>
2023-08-12 17:01:05 -0500

    slider funcional

---
+++ views/js/slider.js
@@ -0,0 +1,9 @@
+function scrolll(){
+    var right = document.querySelector(".scroll-images");
+    right.scrollBy(1000,0)
+}
+function scrollr(){
+    var left = document.querySelector(".scroll-images");
+    left.scrollBy(-1000,0)
+}
+
