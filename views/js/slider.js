dd75732c92b7609a9909479fbde21351afa04a2d
jeffer <jefferdcondiza@gmail.com>
2023-08-12 20:09:53 -0500

    se realizo el hover y se soluciono el tamaño de todo

--- views/js/slider.js
+++ views/js/slider.js
@@ -1,9 +1,9 @@
 function scrolll(){
-    var right = document.querySelector(".scroll-images");
-    right.scrollBy(1000,0)
+    var left = document.querySelector(".scroll-images");
+    left.scrollBy(1000,0)
 }
 function scrollr(){
-    var left = document.querySelector(".scroll-images");
-    left.scrollBy(-1000,0)
+    var right = document.querySelector(".scroll-images");
+    right.scrollBy(-1000,0)
 }
 
