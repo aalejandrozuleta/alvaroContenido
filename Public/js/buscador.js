84c9263011ae974cf53cbf76d78b2bc9e0b090c6
aalejandrozuleta <jhoitan266@gmail.com>
2023-08-16 15:15:44 -0500

    funcionalidad de buscador

---
+++ Public/js/buscador.js
@@ -0,0 +1,36 @@
+function buscarPeliculas() {
+    const input = document.getElementById("buscador");
+    const searchText = input.value.toLowerCase();
+    const peliculas = document.querySelectorAll(".imageDrama h3");
+    const peliculasUnicas = new Set();
+    
+    peliculas.forEach((pelicula) => {
+        const titulo = pelicula.textContent.toLowerCase();
+        if (titulo.includes(searchText)) {
+            const figura = pelicula.closest(".imageDrama");
+            peliculasUnicas.add(figura.outerHTML);
+        }
+    });
+    const peliculasHTML = Array.from(peliculasUnicas).join("");
+    
+    if (peliculasHTML) {
+        abrirModal(peliculasHTML);
+    }
+}
+
+
+
+
+function abrirModal(peliculasHTML) {
+    const modal = document.getElementById("modal");
+    const modalContent = document.getElementById("modalContent");
+
+    modal.style.display = "block";
+    modalContent.innerHTML = peliculasHTML;
+}
+
+function cerrarModal() {
+    const modal = document.getElementById("modal");
+    modal.style.display = "none";
+}
+
