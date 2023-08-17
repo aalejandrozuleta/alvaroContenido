01cfda85f5fd2d1d8104a927b19a0b3285fca8d7
aalejandrozuleta <jhoitan266@gmail.com>
2023-08-16 23:55:20 -0500

    slider funcional sin responsive

--- Public/js/scroll.js
+++ Public/js/scroll.js
@@ -1,18 +1,52 @@
-const pelis = document.querySelector(".pelis");
-const images = pelis.querySelectorAll(".imageDrama");
-let currentIndex = 0;
-
-function scrollIzquierda() {
-    currentIndex = (currentIndex - 2 + images.length) % images.length;
-    updateSlider();
-}
-
-function scrollDerecha() {
-    currentIndex = (currentIndex + 2) % images.length;
-    updateSlider();
-}
-
-function updateSlider() {
+// Define the updateSlider function
+function updateSlider(container, currentIndex) {
     const translateValue = -currentIndex * 9.66;
-    pelis.style.transform = `translateX(${translateValue}%)`;
+    container.style.transform = `translateX(${translateValue}%)`;
 }
+
+// Drama section
+const dramaImages = document.querySelectorAll("#imageDramaContainer .imageDrama");
+let dramaCurrentIndex = 0;
+
+function scrollIzquierdaDrama() {
+    dramaCurrentIndex = (dramaCurrentIndex - 2 + dramaImages.length) % dramaImages.length;
+    updateSlider(document.getElementById('imageDramaContainer'), dramaCurrentIndex);
+}
+
+function scrollDerechaDrama() {
+    dramaCurrentIndex = (dramaCurrentIndex + 2) % dramaImages.length;
+    updateSlider(document.getElementById('imageDramaContainer'), dramaCurrentIndex);
+}
+
+// Comedia section
+const comediaImages = document.querySelectorAll("#imageComediaContainer .imageDrama");
+let comediaCurrentIndex = 0;
+
+function scrollIzquierdaComedia() {
+    comediaCurrentIndex = (comediaCurrentIndex - 2 + comediaImages.length) % comediaImages.length;
+    updateSlider(document.getElementById('imageComediaContainer'), comediaCurrentIndex);
+}
+
+function scrollDerechaComedia() {
+    comediaCurrentIndex = (comediaCurrentIndex + 2) % comediaImages.length;
+    updateSlider(document.getElementById('imageComediaContainer'), comediaCurrentIndex);
+}
+
+// Animación section
+const animacionImages = document.querySelectorAll("#imageAnimacionContainer .imageDrama");
+let animacionCurrentIndex = 0;
+
+function scrollIzquierdaAnimacion() {
+    animacionCurrentIndex = (animacionCurrentIndex - 2 + animacionImages.length) % animacionImages.length;
+    updateSlider(document.getElementById('imageAnimacionContainer'), animacionCurrentIndex);
+}
+
+function scrollDerechaAnimacion() {
+    animacionCurrentIndex = (animacionCurrentIndex + 2) % animacionImages.length;
+    updateSlider(document.getElementById('imageAnimacionContainer'), animacionCurrentIndex);
+}
+
+// Repeat the same structure for other genre sections if needed
+
+
+// Repeat the same structure for other genres
