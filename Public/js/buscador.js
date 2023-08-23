aa116a8e1d13ea4e68d355e4bac450c02b263b06
aalejandrozuleta <jhoitan266@gmail.com>
2023-08-22 20:39:36 -0500

    buscador funcional

--- Public/js/buscador.js
+++ Public/js/buscador.js
@@ -1,17 +1,19 @@
 function buscarPeliculas() {
-    const input = document.getElementsByClassName(".seeker");
+    const input = document.querySelector(".seeker"); 
     const searchText = input.value.toLowerCase();
     const peliculas = document.querySelectorAll(".imageDrama h3");
     const peliculasUnicas = new Set();
-    peliculas.forEach((pelicula) => {
+
+    Array.from(peliculas).forEach((pelicula) => {
         const titulo = pelicula.textContent.toLowerCase();
         if (titulo.includes(searchText)) {
-            const figura = pelicula.closest(".imageDrama");
+            const figura = pelicula.closest("figure.imageDrama");
             peliculasUnicas.add(figura.outerHTML);
         }
     });
+
     const peliculasHTML = Array.from(peliculasUnicas).join("");
-    
+
     if (peliculasHTML) {
         abrirModal(peliculasHTML);
     }
